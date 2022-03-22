faa4960016e92c83a83c103560a866c17363492e
Rheotsu <41465343+Rheotsu@users.noreply.github.com>
2022-03-22 11:59:41 +0530

    Add files via upload

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+namespace WebAPIDIT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!!");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
